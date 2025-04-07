cc19df308ed992aa39305bdf9764888cf3b2e93a
Sp1ker2 <bogdan.speaker@gmail.com>
2025-04-07 17:23:34 +0300

    first task

---
+++ task_1/t_2_2.py
@@ -0,0 +1,19 @@
+
+number = int(input("enter first number : "))
+number_2 = int(input("enter second number : "))
+znak = str(input(" enter + - / * // ** % "))
+match znak:
+    case "+":
+        print(f"{number+number_2}   ")
+    case "*":
+        print(f"{number * number_2}  ")
+    case "-":
+        print(f"{number - number_2}  ")
+    case "/":
+        print(f"{number / number_2} ")
+    case "//":
+        print(f"{number // number_2}  ")
+    case "**":
+        print(f"{number ** number_2}  ")
+    case "%":
+        print(f"{number % number_2}  ")
