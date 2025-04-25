52f9617a50b921231989a5dd297849efef598024
Sp1ker2 <bogdan.speaker@gmail.com>
2025-04-25 18:41:25 +0300

    Add new Python scripts with basic functions and logic
    Introduce `t_6_1.py` to print numbers based on divisibility by 2 or 3. Include `t_6_2.py` with a function to greet based on a specific name or return an error.

---
+++ task_6/t_6_1.py
@@ -0,0 +1,10 @@
+def rangee_10():
+    for i in range(10):
+        if i % 2 == 0:
+            print(i, "делится на 2")
+        elif i % 3 == 0:
+            print(i, "делится на 3")
+        else:
+            print(i, "не делится на 2 и на 3")
+
+rangee_10()
---
+++ task_6/t_6_2.py
@@ -0,0 +1,9 @@
+def first(name):
+    while True:
+        if name == "First":
+            print("Hello, First!")
+            break
+        else:
+            print("Error")
+            break
+first("ffff")
