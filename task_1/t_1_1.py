f131cb76b545809fa86700811a574df19c3d5c1f
Sp1ker2 <bogdan.speaker@gmail.com>
2025-04-26 09:18:04 +0300

    t

--- task_1/t_1_1.py
+++ task_1/t_1_1.py
@@ -2,4 +2,4 @@
 name = str(input("What is your name? "))
 age = int(input("How old are you? "))
 city = str(input("Where do you live? "))
-print(f"Hello, {name}. Your age is {age} . You live in  {city}" )
+print(f"Hello, {name}. Your age is {age}& You live in  {city}" )
--- task_1/t_2_2.py
+++ task_1/t_2_2.py
@@ -4,7 +4,7 @@ number_2 = int(input("enter second number : "))
 znak = str(input(" enter + - / * // ** % "))
 match znak:
     case "+":
-        print(f"{number+number_2}   ")
+        print(f"{number+number_2} ")
     case "*":
         print(f"{number * number_2}  ")
     case "-":
